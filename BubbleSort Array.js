29cd603fc99b8731f2c3457d91180d99e2fb1182
oromero270 <oromero2018@gmail.com>
2021-08-30 10:46:06 -0500

    this is algo

--- BubbleSort Array.js
+++ BubbleSort Array.js
@@ -2,14 +2,12 @@ function bubbleSort(arr){
     for (let  i=0 ; i<arr.length; i++){
         for (let j =0; j< (arr.length -i-1);j++){
         if(arr[j]>arr[j+1]){
-            let tmp = arr[j];
-            arr[j]= arr[j+1];
-            arr[j+1]=tmp;
-        }
+        [arr[j],arr[j+1]] =[arr[j+1], arr[j]]
+        }              
     }
     
     }
 console.log(arr);
 }
 
-bubbleSort([23,12,18,22,21,1,13,25])
+bubbleSort([23,12,18,22,21,1,13,25,50])
